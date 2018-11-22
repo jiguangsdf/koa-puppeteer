35f9e55d47b7980ee86b657ed17289c2487d9b89
jiguang7 <jiguang7c@gmail.com>
2018-11-23 05:00:53 +0800

    更新部署方式

--- docker.sh
+++ docker.sh
@@ -9,5 +9,6 @@ git clone https://github.com/jiguang7/koa-puppeteer.git
 docker pull alekzonder/puppeteer
 
 # run images
+# 务必在当起目录下，也就是git clone的地方
 
-docker run -d --name puppeteer -p 127.0.0.1:3200:3200 -v $PWD/chromeheadless/:/app/  b7407aba3236
+docker run -d --name puppeteer -p 127.0.0.1:3200:3200 -v $PWD/koa-puppeteer/:/app/  alekzonder/puppeteer
